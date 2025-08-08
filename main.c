667f7a862c61323d9d80ef52e3e2a715157ab209
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-08-08 13:08:03 +0100

    Updates error handling

--- main.c
+++ main.c
@@ -1,44 +1,83 @@
-#include <setjmp.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-jmp_buf exception_buffer;
-
-enum {
+typedef enum {
+    ARGUMENTS_PARSING_SUCCESS = 0,
     TARGET_DIRECTORY_NOT_PASSED = 1,
     INVALID_COMMAND_PASSED = 2,
-};
+} arguments_parsing_error;
 
-void throw_exception(const int error_code) {
-    longjmp(exception_buffer, error_code);
-}
+typedef enum {
+    FILE_PARSING_SUCCESS = 0,
+    ERROR_OPENING_FILE = 1,
+    ERROR_PARSING_LINE = 2,
+} file_parsing_error;
 
-char *get_target_directory(const int number_of_arguments, char *arguments[]) {
+arguments_parsing_error parse_arguments(const int number_of_arguments, char *arguments[], char **tar_dir) {
     if (number_of_arguments < 2) {
-        throw_exception(TARGET_DIRECTORY_NOT_PASSED);
+        return TARGET_DIRECTORY_NOT_PASSED;
     }
 
     char *string = arguments[1];
     const size_t length = strlen(string);
     if (length < 3 || string[0] != '-' || string[1] != '-') {
-        throw_exception(INVALID_COMMAND_PASSED);
+        return INVALID_COMMAND_PASSED;
     }
 
     char *target_directory = string + 2;
     if (*target_directory == '\0') {
-        throw_exception(INVALID_COMMAND_PASSED);
+        return INVALID_COMMAND_PASSED;
     }
 
-    return target_directory;
+    *tar_dir = target_directory;
+    return ARGUMENTS_PARSING_SUCCESS;
+}
+
+int parse_line(const char *line) {
+    const size_t length = strlen(line);
+
+    if (length < 2) {
+        return 1;
+    }
+
+    // Parse file here
+
+    return 0;
+}
+
+file_parsing_error parse_file(const char *target_directory) {
+    FILE *file = fopen(target_directory, "r");
+    if (file == NULL) {
+        return ERROR_OPENING_FILE;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+
+    while (getline(&line, &len, file) != -1) {
+        const int line_parsing_error = parse_line(line);
+        if (line_parsing_error != 0) {
+            return ERROR_PARSING_LINE;
+        }
+    }
+
+    free(line);
+    fclose(file);
+
+    return FILE_PARSING_SUCCESS;
 }
 
 int main(const int argc, char *args[]) {
-    int exception_code = setjmp(exception_buffer);
-    if (exception_code == 0) {
-        char *target_directory = get_target_directory(argc, args);
-        printf("%s\n", target_directory);
-    } else {
-        printf("Exception code: %d\n", exception_code);
+    char *target_directory = NULL;
+    const arguments_parsing_error a_error = parse_arguments(argc, args, &target_directory);
+    if (a_error != ARGUMENTS_PARSING_SUCCESS) {
+        return a_error;
+    }
+
+    const file_parsing_error f_error = parse_file(target_directory);
+    if (f_error != FILE_PARSING_SUCCESS) {
+        return f_error;
     }
 
     return 0;
